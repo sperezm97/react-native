8f71799549c9ec0b25ab14ecb2d7788ac67e2df2
masterial <konstantin.k.yurchenko@gmail.com>
2021-03-04 17:03:25 -0800

    Fixes decrypt functionality.

--- src/crypto/aes.ts
+++ src/crypto/aes.ts
@@ -1,12 +1,13 @@
-import RNCryptor from 'rn-rncryptor'
-
+// import RNCryptor from 'rn-rncryptor'
 // https://github.com/Evanfeenstra/rn-rncryptor
+
+import RNCryptor from 'react-native-rncryptor';
 
 export async function encrypt(txt, pwd): Promise<string> {
   try {
-    const encryptedbase64 = await RNCryptor.encrypt(txt,pwd)
+    const encryptedbase64 = await RNCryptor.encrypt(txt, pwd)
     return encryptedbase64
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -14,9 +15,9 @@ export async function encrypt(txt, pwd): Promise<string> {
 
 export async function encryptFile(pathname, pwd): Promise<string> {
   try {
-    const encryptedbase64 = await RNCryptor.encryptFile(pathname,pwd)
+    const encryptedbase64 = await RNCryptor.encryptFile(pathname, pwd)
     return encryptedbase64
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -24,9 +25,9 @@ export async function encryptFile(pathname, pwd): Promise<string> {
 
 export async function encryptFromBase64(b64, pwd): Promise<string> {
   try {
-    const encryptedbase64 = await RNCryptor.encryptFromBase64(b64,pwd)
+    const encryptedbase64 = await RNCryptor.encryptFromBase64(b64, pwd)
     return encryptedbase64
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -34,9 +35,9 @@ export async function encryptFromBase64(b64, pwd): Promise<string> {
 
 export async function decrypt(txt, pwd): Promise<string> {
   try {
-    const dec = await RNCryptor.decrypt(txt,pwd)
+    const dec = await RNCryptor.decrypt(txt, pwd)
     return dec
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -44,9 +45,9 @@ export async function decrypt(txt, pwd): Promise<string> {
 
 export async function decryptToBase64(txt, pwd): Promise<string> {
   try {
-    const decryptedbase64 = await RNCryptor.decryptToBase64(txt,pwd)
+    const decryptedbase64 = await RNCryptor.decryptToBase64(txt, pwd)
     return decryptedbase64
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -54,9 +55,9 @@ export async function decryptToBase64(txt, pwd): Promise<string> {
 
 export async function readEncryptedFile(filepath, pwd): Promise<string> {
   try {
-    const decryptedbase64 = await RNCryptor.readEncryptedFile(filepath,pwd)
+    const decryptedbase64 = await RNCryptor.readEncryptedFile(filepath, pwd)
     return decryptedbase64
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -64,9 +65,9 @@ export async function readEncryptedFile(filepath, pwd): Promise<string> {
 
 export async function decryptFileAndSave(filepath, pwd, extension): Promise<string> {
   try {
-    const path = await RNCryptor.decryptFileAndSave(filepath,pwd,extension)
+    const path = await RNCryptor.decryptFileAndSave(filepath, pwd, extension)
     return path
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
@@ -74,18 +75,18 @@ export async function decryptFileAndSave(filepath, pwd, extension): Promise<stri
 
 export async function decryptFileAndSaveReturningContent(filepath, pwd, extension): Promise<string> {
   try {
-    const content = await RNCryptor.decryptFileAndSaveReturningContent(filepath,pwd,extension)
+    const content = await RNCryptor.decryptFileAndSaveReturningContent(filepath, pwd, extension)
     return content
-  } catch(e){
+  } catch (e) {
     console.log(e)
     return ''
   }
 }
 
-async function testAES(){
-  const msg='hi'
-  const enc = await encrypt(msg,'pass')
-  const dec = await decrypt(enc,'pass')
-  console.log(dec,dec===msg)
+async function testAES() {
+  const msg = 'hi'
+  const enc = await encrypt(msg, 'pass')
+  const dec = await decrypt(enc, 'pass')
+  console.log(dec, dec === msg)
 }
 
--- src/crypto/e2e.ts
+++ src/crypto/e2e.ts
@@ -26,32 +26,32 @@ Private keys stored in android/ios keychain
 */
 
 // encrypt text with a public key
-export async function encryptPublic(text:string, key:string) {
+export async function encryptPublic(text: string, key: string) {
     return await rsa.encrypt(text, key)
 }
 
 // decrypt with your own PRIVATE key (from keychain)
-export async function decryptPrivate(base64:string) {
+export async function decryptPrivate(base64: string) {
     return await rsa.decrypt(base64)
 }
 
 // encrypt with a symmetric password (using RN-Cryptor)
-export async function encrypt(text:string, pwd:string) {
+export async function encrypt(text: string, pwd: string) {
     return await aes.encrypt(text, pwd)
 }
 
 // decrypt with a symmetric password (using RN-Cryptor)
-export async function decrypt(text:string, pwd:string) {
+export async function decrypt(text: string, pwd: string) {
     return await aes.decrypt(text, pwd)
 }
 
 // encrypt file with symmetric password
-export async function encryptFile(filepath:string, pwd:string) {
+export async function encryptFile(filepath: string, pwd: string) {
     return await aes.encryptFile(filepath, pwd)
 }
 
 // encrypt file with symmetric password from base64
-export async function encryptFileFromBase64(b64:string, pwd:string) {
+export async function encryptFileFromBase64(b64: string, pwd: string) {
     return await aes.encryptFromBase64(b64, pwd)
 }
 
