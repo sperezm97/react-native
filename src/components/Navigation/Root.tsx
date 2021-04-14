ba67d5cc5f50b3e841251763baa1fce842f01862
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2021-04-14 01:01:55 -0700

    fix: add chat routing fix

--- src/components/Navigation/Root.tsx
+++ src/components/Navigation/Root.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { createStackNavigator } from '@react-navigation/stack'
-import { TransitionSpecs, TransitionPresets } from '@react-navigation/stack'
 
 import Chats from '../chat/Navigation'
 import Account from '../Account/Navigation'
