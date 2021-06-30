62a3afcc17ed96fc980938c68d153df98a56a21b
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2021-06-30 16:53:11 -0700

    feat: build wallet new UI

--- src/components/Payment/Header.tsx
+++ src/components/Payment/Header.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import { Appbar, IconButton, ActivityIndicator } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native'
 import { useObserver } from 'mobx-react-lite'
@@ -7,18 +7,39 @@ import Toast from 'react-native-simple-toast'
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import { useStores, useTheme } from '../../store'
+import * as utils from '../utils/utils'
 import Pushable from '../common/Pushable'
 import Button from '../common/Button'
+import Typography from '../common/Typography'
+import DialogWrap from '../common/Dialogs/DialogWrap'
 
 export default function Header({ onScanClick }) {
   const navigation = useNavigation()
-  const { ui } = useStores()
+  const { ui, details, user } = useStores()
   const theme = useTheme()
+  const [capacityDialog, setCapacityDialog] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [requestSent, setRequestSent] = useState(false)
 
   const showStatusHandler = () => {
     const status = ui.connected ? 'Connected node' : 'Disconnected node'
 
     Toast.showWithGravity(status, 0.4, Toast.CENTER)
+  }
+
+  async function onCapacityRequest() {
+    try {
+      setLoading(true)
+      const done = await details.requestCapacity(user.publicKey)
+      await utils.sleep(300)
+
+      if (done) {
+        setRequestSent(true)
+      }
+    } catch (error) {
+    } finally {
+      setLoading(false)
+    }
   }
 
   return useObserver(() => {
@@ -31,27 +52,89 @@ export default function Header({ onScanClick }) {
             </Pushable>
           </View>
           <View style={{ ...styles.flex, ...styles.right }}>
-            {/* {ui.loadingHistory ? (
-              <ActivityIndicator animating={true} color={theme.grey} size={18} style={{}} />
-            ) : (
-              <TouchableOpacity onPress={showStatusHandler}>
-                <MaterialIcon name='lightning-bolt' size={20} color={ui.connected ? '#49ca97' : '#febd59'} />
-              </TouchableOpacity>
-            )} */}
-
             <TouchableOpacity
-              onPress={() => navigation.navigate('AddSats')}
+              style={{ flexDirection: 'row', alignItems: 'center' }}
+              onPress={() => setCapacityDialog(true)}
+              // onPress={() => navigation.navigate('AddSats')}
               activeOpacity={0.6}
             >
-              <Button
-                mode='text'
-                icon={() => <MaterialIcon name='plus' color={theme.primary} size={20} />}
-                fs={14}
-              >
-                Add Sats
-              </Button>
+              <MaterialIcon name='plus' color={theme.primary} size={20} />
+              <Typography style={{ marginLeft: 2 }} size={16} color={theme.primary}>
+                Add Capacity
+              </Typography>
             </TouchableOpacity>
           </View>
+
+          <DialogWrap
+            visible={capacityDialog}
+            onDismiss={() => setCapacityDialog(false)}
+            // minH={190}
+          >
+            <Typography textAlign='center' size={17}>
+              Request an increase of capacity.
+            </Typography>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+              {requestSent ? (
+                <View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      marginTop: 25
+                    }}
+                  >
+                    <MaterialIcon name='check' color={theme.green} size={22} />
+                    <Typography
+                      textAlign='center'
+                      size={17}
+                      color={theme.title}
+                      style={{ marginLeft: 5 }}
+                    >
+                      Your request has been sent.
+                    </Typography>
+                  </View>
+                  <View style={{ alignItems: 'center' }}>
+                    <Button
+                      mode='text'
+                      size='small'
+                      h={40}
+                      fs={12}
+                      w={100}
+                      onPress={() => {
+                        setCapacityDialog(false)
+                        setRequestSent(false)
+                      }}
+                      style={{
+                        marginTop: 25
+                        // borderWidth: 1,
+                        // borderColor: theme.greyPrimary
+                      }}
+                    >
+                      Ok
+                    </Button>
+                  </View>
+                </View>
+              ) : (
+                <Button
+                  color={theme.darkPrimary}
+                  // mode='text'
+                  size='small'
+                  h={40}
+                  fs={12}
+                  loading={loading}
+                  w={160}
+                  onPress={onCapacityRequest}
+                  style={{
+                    marginTop: 25,
+                    borderWidth: 1,
+                    borderColor: theme.greyPrimary
+                  }}
+                >
+                  Confirm
+                </Button>
+              )}
+            </View>
+          </DialogWrap>
         </View>
       </Appbar.Header>
     )
@@ -77,7 +160,7 @@ const styles = StyleSheet.create({
     marginLeft: 0
   },
   right: {
-    // marginRight: 12,
+    marginRight: 12,
     justifyContent: 'flex-end'
   }
 })
--- src/main.tsx
+++ src/main.tsx
@@ -67,20 +67,16 @@ export default function Main() {
   }
 
   async function createPrivateKeyIfNotExists(contacts, user) {
-    console.log("=== createPrivateKeyIfNotExists ===")
+    console.log('=== createPrivateKeyIfNotExists ===')
     // const priv = null
     const priv = await rsa.getPrivateKey()
     const me = contacts.contacts.find(c => c.id === user.myid)
-    console.log("priv:", priv)
-    console.log("me:", me)
-    console.log("me.contact_key:", me.contact_key)
-
 
     // private key has been made
     if (priv) {
       // set into user.contactKey
       if (me && me.contact_key) {
-        console.log("Is here?")
+        console.log('Is here?')
         if (!user.contactKey) {
           user.setContactKey(me.contact_key)
         }
@@ -88,7 +84,7 @@ export default function Main() {
         // set into me Contact
       } else if (user.contactKey) {
       } else {
-        console.log("JASDLKJASDLKJASDLKJADS")
+        console.log('JASDLKJASDLKJASDLKJADS')
         // need to regen :(
         const keyPair = await rsa.generateKeyPair()
         user.setContactKey(keyPair.public)
@@ -100,11 +96,11 @@ export default function Main() {
       }
       // no private key!!
     } else {
-      console.log("WITHOUT PRIVATE KEY")
+      console.log('WITHOUT PRIVATE KEY')
       const keyPair = await rsa.generateKeyPair()
       user.setContactKey(keyPair.public)
-      console.log("keyPair:", keyPair)
-      console.log("keyPair.public:", keyPair.public)
+      console.log('keyPair:', keyPair)
+      console.log('keyPair.public:', keyPair.public)
       contacts.updateContact(user.myid, {
         contact_key: keyPair.public
       })
