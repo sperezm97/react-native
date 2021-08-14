6a23b03f39d3aba2db326212eb0d48f68229cc57
Antoniel Magalhães <antoniel2210@gmail.com>
2021-08-14 17:16:52 -0300

    feat: add button asking to user pay a embed video

--- src/components/chat/msg/embedVideo.tsx
+++ src/components/chat/msg/embedVideo.tsx
@@ -1,4 +1,9 @@
 import React, { useMemo } from 'react'
+import { View, Text } from 'react-native'
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useTheme } from '../../../store'
+import Button from '../../common/Button'
+import Typography from '../../common/Typography'
 
 import WebViewVideo from './webViewVideo'
 import { getYoutubeVideoID } from './utils'
@@ -18,7 +23,40 @@ const EmbedVideo: React.FC<EmbedVideoTypes> = ({ type, link, onLongPress }) => {
     return `https://www.youtube.com/embed/${youtubeVideoID}`
   }, [link, type])
 
-  return !!embedLink && <WebViewVideo embedLink={embedLink} onLongPress={onLongPress} />
+  return (!!embedLink ?
+    <PaidEmbedWrapper>
+      <WebViewVideo embedLink={embedLink} onLongPress={onLongPress} />
+    </PaidEmbedWrapper> : null)
 }
 
+const PaidEmbedWrapper = ({ children }) => {
+  const theme = useTheme()
+  const isMessageMissingPayment = false // TODO: check if the user already paid the video
+  const amountToPay = 50 // TODO: Bring the value of payment
+  return (
+    <View>
+      {children}
+      {/* Displays a button do enable user buy this content */}
+      {isMessageMissingPayment && 
+              <Button
+                color={theme.dark ? theme.primary : theme.main}
+                round={0}
+                onPress={() => console.log("TODO")}
+                style={{borderBottomLeftRadius: 25, borderBottomRightRadius: 25}}
+                loading={false} // TODO
+                icn={() => (
+                  <MaterialCommunityIcon
+                    name="arrow-top-right"
+                    color={theme.dark ? theme.white : theme.icon}
+                    direction="ltr"
+                    size={18}
+                  />
+                )}
+              >
+                <Typography size={12}>{`Pay ${amountToPay} sat`}</Typography>
+              </Button>
+}
+    </View>
+  )
+}
 export default EmbedVideo
