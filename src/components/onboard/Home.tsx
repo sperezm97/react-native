02af328e2d73fd8d7505e22e6592e9dfda27eeec
masterial <konstantin.k.yurchenko@gmail.com>
2021-08-28 09:44:38 -0700

    Fixes typo.

--- src/components/onboard/Home.tsx
+++ src/components/onboard/Home.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
-import RadialGradient from 'react-native-radial-gradient'
-import { useNavigation } from '@react-navigation/native'
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import RadialGradient from "react-native-radial-gradient";
+import { useNavigation } from "@react-navigation/native";
 
-import { useTheme } from '../../store'
-import Typography from '../common/Typography'
-import Button from '../common/Button'
+import { useTheme } from "../../store";
+import Typography from "../common/Typography";
+import Button from "../common/Button";
 
 export default function Home() {
-  const theme = useTheme()
-  const navigation = useNavigation()
+  const theme = useTheme();
+  const navigation = useNavigation();
 
   return (
-    <View style={{ ...styles.wrap }} accessibilityLabel='onboard-code'>
+    <View style={{ ...styles.wrap }} accessibilityLabel="onboard-code">
       <RadialGradient
         style={styles.gradient}
         colors={[theme.orange, theme.orangeSecondary]}
@@ -21,79 +21,88 @@ export default function Home() {
         radius={400}
       >
         <View style={styles.content}>
-          <View style={{ ...styles.imageWrapper, backgroundColor: theme.transparent }}>
+          <View
+            style={{
+              ...styles.imageWrapper,
+              backgroundColor: theme.transparent,
+            }}
+          >
             <Image
-              source={require('../../assets/zion-dark-theme.png')}
+              source={require("../../assets/zion-dark-theme.png")}
               style={{ width: 140, height: 100 }}
-              resizeMode={'contain'}
+              resizeMode={"contain"}
             />
           </View>
 
           <Button
             color={theme.orangeSecondary}
-            w='70%'
-            size='large'
+            w="70%"
+            size="large"
             style={{
               borderWidth: 2,
-              borderColor: theme.white
+              borderColor: theme.white,
             }}
-            onPress={() => navigation.navigate('Invite')}
+            onPress={() => navigation.navigate("Invite")}
           >
-            <Typography color={theme.white} fw='700'>
+            <Typography color={theme.white} fw="700">
               Subscribe to the waitlist
             </Typography>
           </Button>
           <Button
-            fw='500'
+            fw="500"
             color={theme.lightGrey}
-            w='70%'
+            w="70%"
             style={{ marginTop: 15 }}
-            onPress={() => navigation.navigate('Onboard', { codeType: 'invite' })}
+            onPress={() =>
+              navigation.navigate("Onboard", { codeType: "invite" })
+            }
           >
-            I have a invite code
+            I have an invite code
           </Button>
           <Button
-            fw='500'
+            fw="500"
             color={theme.lightGrey}
-            w='70%'
+            w="70%"
             style={{ marginTop: 15 }}
-            onPress={() => navigation.navigate('Onboard', { codeType: 'backup' })}
+            onPress={() =>
+              navigation.navigate("Onboard", { codeType: "backup" })
+            }
           >
             I have a backup code
           </Button>
         </View>
       </RadialGradient>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   wrap: {
     flex: 1,
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0
+    bottom: 0,
   },
   gradient: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%'
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
   },
   content: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%'
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
   },
   imageWrapper: {
     paddingHorizontal: 30,
     borderRadius: 30,
     marginBottom: 45,
-  }
-})
+  },
+});
