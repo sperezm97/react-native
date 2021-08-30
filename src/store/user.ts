54e05be14657caaa838812448846ddb5fbb3155d
Lucas Floriani <lucas.floriane@gmail.com>
2021-08-30 09:50:23 -0300

    feat: create connection to the shop api

--- src/store/user.ts
+++ src/store/user.ts
@@ -273,10 +273,10 @@ class UserStore {
     try {
       if (!email) return
 
-      const r = await api.invite.post('invite_request', { email }, '', { rawValue: true })
+      const r = await api.shop.post('invite_request', { email }, '', { rawValue: true })
       if (!r) throw new Error('Failed');
 
-      return { status: 'ok', payload: { id: r.invite.id, duplicate: !!r.invite.check } }
+      return { status: 'ok', payload: { id: r?.invite?.id, duplicate: !!r?.exist } }
     } catch (error) {
       console.log('error store', error)
       return { status: 'failure' }
