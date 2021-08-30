c93c3d2d6435e093fbd97960a4608fa1cca12c31
Antoniel Magalhães <antoniel2210@gmail.com>
2021-08-30 12:32:01 -0300

    refactor: move `recordingCircle`

--- src/components/chat/BottomBar/RecordingBottomBar.tsx
+++ src/components/chat/BottomBar/RecordingBottomBar.tsx
@@ -14,21 +14,29 @@ export const RecordingBottomBar: React.FC<IRecordingBottomBar> = ({
   const theme = useTheme();
   if (!recordingStartTime) return null;
   return (
-    <View style={styles.recording}>
-      <RecDot />
-      <View style={styles.recordSecs}>
-        <Text style={{ ...styles.recordSecsText, color: theme.title }}>
-          {recordSecs}
-        </Text>
+    <>
+      <View style={styles.recording}>
+        <RecDot />
+        <View style={styles.recordSecs}>
+          <Text style={{ ...styles.recordSecsText, color: theme.title }}>
+            {recordSecs}
+          </Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Icon name="rewind" size={16} color="grey" />
+          <Text style={{ marginLeft: 5, color: theme.subtitle }}>
+            Swipe to cancel
+          </Text>
+        </View>
       </View>
-      <View
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-      >
-        <Icon name="rewind" size={16} color="grey" />
-        <Text style={{ marginLeft: 5, color: theme.subtitle }}>
-          Swipe to cancel
-        </Text>
-      </View>
-    </View>
+      {/* This is a spinner like, that appears bellow mic icon on recordingBottomBar */}
+      <View style={styles.recordingCircle} />
+    </>
   );
 };
