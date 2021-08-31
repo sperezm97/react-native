3d6f0f2e0d466b6bf40cf7e0632d04c04eb8642b
Antoniel Magalhães <antoniel2210@gmail.com>
2021-08-31 15:20:28 -0300

    refactor: use observer on `Code`

--- src/components/onboard/index.tsx
+++ src/components/onboard/index.tsx
@@ -1,74 +1,71 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import { useObserver } from 'mobx-react-lite'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { observer } from "mobx-react-lite";
 
-import { useStores } from '../../store'
-import Code from './code'
-import Welcome from './welcome'
-import NameAndKey from './nameAndKey'
-import Ready from './ready'
-import PIN from './choosePIN'
-import ProfilePic from './profilePic'
-import SuggestToUserToBackupTheirKeys from './suggestToUserToBackupTheirKeys'
-// import Backup from './Backup'
+import { useStores } from "../../store";
+import Code from "./code";
+import Welcome from "./welcome";
+import NameAndKey from "./nameAndKey";
+import Ready from "./ready";
+import PIN from "./choosePIN";
+import ProfilePic from "./profilePic";
+import SuggestToUserToBackupTheirKeys from "./suggestToUserToBackupTheirKeys";
+// TODO: remove this lien import Backup from './Backup'
 
-/*
-1. scan or enter code, create ip (from invite server), create auth_token in Relay
-2. create inviter contact (relay)
-3. set pin
-4. set my nickname (and RSA pubkey!)
-5. SuggestToUserToBackupTheirKeys
-6. set my profile pic
-7. Ready!
-*/
+const steps = [
+  Code, // scan or enter code, create ip (from invite server), create auth_token in Relay
+  Welcome, // create inviter contact (relay)
+  PIN, // set pin
+  NameAndKey, // set my nickname (and RSA pubkey!)
+  SuggestToUserToBackupTheirKeys, // Presents a a video saying to user backup their keys
+  ProfilePic, // SuggestToUserToBackupTheirKeys
+  Ready, // set my profile pic
+];
 
-// Final: ProfilePic before Ready
-const steps = [Code, Welcome, PIN, NameAndKey, SuggestToUserToBackupTheirKeys ,ProfilePic, Ready]
-export default function OnBoard() {
-  const { ui, user } = useStores()
+const OnBoard = observer(() => {
+  const { ui, user } = useStores();
 
-  return useObserver(() => {
-    let step = user.onboardStep
+  let step = user.onboardStep;
 
-    function stepForward() {
-      if (step >= steps.length - 1) {
-        onFinish()
-      } else {
-        user.setOnboardStep(step + 1)
-      }
+  function stepForward() {
+    if (step >= steps.length - 1) {
+      onFinish();
+    } else {
+      user.setOnboardStep(step + 1);
     }
-    function stepBack() {
-      user.setOnboardStep(step - 1)
-    }
+  }
+  function stepBack() {
+    user.setOnboardStep(step - 1);
+  }
 
-    function onFinish() {
-      user.finishOnboard() // clear out things
-      ui.setSignedUp(true) // signed up w key export
-      ui.setPinCodeModal(true) // also PIN has been set
-    }
+  function onFinish() {
+    user.finishOnboard(); // clear out things
+    ui.setSignedUp(true); // signed up w key export
+    ui.setPinCodeModal(true); // also PIN has been set
+  }
 
-    return (
-      <View style={styles.wrap} accessibilityLabel='onboard-wrap'>
-        {steps.map((C, i) => {
-          const render = i === step - 1 || i === step || i === step + 1
-          if (!render) return <View key={i} />
-          return (
-            <C
-              key={i}
-              z={i}
-              show={step > i - 1}
-              isTheMainRender={step===i}
-              onDone={stepForward}
-              onBack={stepBack}
-              onRestore={onFinish}
-            />
-          )
-        })}
-      </View>
-    )
-  })
-}
+  return (
+    <View style={styles.wrap} accessibilityLabel="onboard-wrap">
+      {steps.map((C, i) => {
+        const render = i === step - 1 || i === step || i === step + 1;
+        if (!render) return <View key={i} />;
+        return (
+          <C
+            key={i}
+            z={i}
+            show={step > i - 1}
+            isTheMainRender={step === i}
+            onDone={stepForward}
+            onBack={stepBack}
+            onRestore={onFinish}
+          />
+        );
+      })}
+    </View>
+  );
+});
 
 const styles = StyleSheet.create({
-  wrap: { flex: 1 }
-})
+  wrap: { flex: 1 },
+});
+export default OnBoard;
