c6d2ec9393a5ee1021109d4c9eca2fc9e7920a32
Christopher David <chris@arcade.city>
2021-10-13 11:43:08 -0500

    errorsimple

--- app/views/error/error-simple.tsx
+++ app/views/error/error-simple.tsx
@@ -1,12 +1,10 @@
 import React, { ErrorInfo } from 'react'
-import { TextStyle, View, ViewStyle, ScrollView, ImageStyle } from 'react-native'
-// import { color } from '../../theme'
-import Button from 'components/common/Button'
-import Icon from 'components/common/Icon'
-import Text from 'components/common/Typography'
+import { Text, Button, TextStyle, View, ViewStyle, ScrollView, ImageStyle } from 'react-native'
+// import Button from 'components/common/Button'
+// import Text from 'components/common/Typography'
 
 const CONTAINER: ViewStyle = {
-  alignItems: 'center',
+  // alignItems: 'center',
   flex: 1,
   padding: 16,
   paddingVertical: 50,
@@ -16,7 +14,7 @@ const CONTAINER: ViewStyle = {
 const ERROR_DETAILS_CONTAINER: ViewStyle = {
   width: '100%',
   maxHeight: '60%',
-  backgroundColor: 'red',
+  // backgroundColor: 'red',
   marginVertical: 15,
   paddingHorizontal: 10,
   paddingBottom: 15,
@@ -25,7 +23,6 @@ const ERROR_DETAILS_CONTAINER: ViewStyle = {
 
 const BTN_RESET: ViewStyle = {
   paddingHorizontal: 40,
-
   backgroundColor: 'blue',
 }
 
@@ -33,12 +30,15 @@ const TITLE_ERROR: TextStyle = {
   color: 'red',
   fontWeight: 'bold',
   paddingVertical: 15,
+  marginTop: 30,
+  textAlign: 'center',
 }
 
 const FRIENDLY_SUBTITLE: TextStyle = {
   color: 'black',
   fontWeight: 'normal',
   paddingVertical: 15,
+  textAlign: 'left',
 }
 
 const CONTENT_ERROR: TextStyle = {
@@ -68,26 +68,28 @@ export interface ErrorComponentProps {
 /**
  * Describe your component here
  */
-export const ErrorComponent = (props: ErrorComponentProps) => {
-  console.log(props)
+export const ErrorSimple = (props: any) => {
   return (
     <View style={CONTAINER}>
-      <Text style={TITLE_ERROR} text='App error :(' />
-      <Text
-        style={FRIENDLY_SUBTITLE}
-        text='Your issue has been reported to developers. Please try again later.'
-      />
-      <Text
-        style={FRIENDLY_SUBTITLE}
-        text='You may be able to get around this by closing and reopening your app.'
-      />
+      <Text style={TITLE_ERROR}>App Error :(</Text>
+      <Text style={FRIENDLY_SUBTITLE}>Oops! The app crashed.</Text>
+      <Text style={FRIENDLY_SUBTITLE}>
+        The issue has been reported to developers. Please try again later.
+      </Text>
+      <Text style={FRIENDLY_SUBTITLE}>
+        You may be able to get around the error by closing and reopening your app.
+      </Text>
       <View style={ERROR_DETAILS_CONTAINER}>
-        <ScrollView>
-          <Text selectable style={CONTENT_ERROR} text={`${props.error}`} />
-          <Text selectable style={CONTENT_BACKTRACE} text={`${props.errorInfo.componentStack}`} />
-        </ScrollView>
+        {/* <ScrollView>
+          <Text selectable style={CONTENT_ERROR}>
+            Dummy info
+          </Text>
+          <Text selectable style={CONTENT_ERROR}>
+            Double dummy
+          </Text>
+        </ScrollView> */}
       </View>
-      <Button style={BTN_RESET} onPress={props.onReset} tx='errorScreen.reset' />
+      {/* <Button style={BTN_RESET} onPress={props.onReset} tx='errorScreen.reset' /> */}
     </View>
   )
 }
